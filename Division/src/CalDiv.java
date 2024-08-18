0bc8da7647f6d097149728f9c9e54f3e37d08e2e
saketsn <saketnandan15@gmail.com>
2024-08-18 15:22:31 +0530

    cal file 2nd commit

--- Division/src/CalDiv.java
+++ Division/src/CalDiv.java
@@ -2,5 +2,6 @@ public class CalDiv {
     public static void main(String[] args) {
         int a= 100,b=20;
         System.out.println("Division = "+ (a/b));
+        System.out.println("Multiplication = "+(a*b));
     }
 }
